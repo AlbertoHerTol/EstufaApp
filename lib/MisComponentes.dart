d2e901a41b40d6603b6d2f753f237ee77896f5e8
AlberHerTol <alberto.herrero.toledano@alumnos.uva.es>
2020-11-12 15:46:46 +0100

    Gauge pintado

--- lib/MisComponentes.dart
+++ lib/MisComponentes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; // tools-flutter- flutter pub get
-import 'dart:convert';
+import 'package:syncfusion_flutter_gauges/gauges.dart';// https://pub.dev/packages/syncfusion_flutter_gauges/install // https://pub.dev/packages/syncfusion_flutter_gauges
+
 /*-------------------------------------------------  PLANTILLA STATEFUL
 class Formulario extends StatefulWidget{
   @override
@@ -45,7 +45,9 @@ class InicioState extends State<Inicio>{
     print ("json-->"+objJson.toString());
     print("la temperatura del json es:"+objJson["estufa"]["estado"]["TemperaturaAmbiente"].toString());
     print ("la variable temperatura es");
+
     temperatura=objJson["estufa"]["estado"]["TemperaturaAmbiente"].toString();
+
   }
 
   InicioState(var json){
@@ -65,9 +67,28 @@ class InicioState extends State<Inicio>{
           Column(
             children: [
               Text("VENTANA DE INICIO"),
-              Text("La temperatura es: "+temperatura)
-              // https://pub.dev/packages/syncfusion_flutter_gauges/install
-              // https://pub.dev/packages/syncfusion_flutter_gauges
+              Text("La temperatura es: "+temperatura),
+              SfRadialGauge(
+                      axes: <RadialAxis>[
+                        RadialAxis(minimum: 0, maximum: 40,
+                            ranges: <GaugeRange>[
+                              GaugeRange(startValue: 0, endValue: 5, color:Colors.lightBlueAccent),
+                              GaugeRange(startValue: 5, endValue: 10, color:Colors.blue),
+                              GaugeRange(startValue: 10,endValue: 15,color: Colors.lightGreenAccent),
+                              GaugeRange(startValue: 15,endValue: 20,color: Colors.greenAccent),
+                              GaugeRange(startValue: 20,endValue: 25,color: Colors.green),
+                              GaugeRange(startValue: 25,endValue: 30,color: Colors.yellowAccent),
+                              GaugeRange(startValue: 30,endValue: 35,color: Colors.orangeAccent),
+                              GaugeRange(startValue: 35,endValue: 40,color: Colors.red),
+                            ],
+                            pointers: <GaugePointer>[
+                              NeedlePointer(value: 90)],
+                            annotations: <GaugeAnnotation>[
+                              GaugeAnnotation(widget: Container(child:
+                              Text('90.0',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold))),
+                                  angle: 90, positionFactor: 0.5
+                              )]
+                        )])
             ],
           ),
     );
